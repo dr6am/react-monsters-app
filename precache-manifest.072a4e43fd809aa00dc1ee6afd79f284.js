cd18904ddefa6117497597c8776a90709ae996ab
Alex <alexanderbasoc@gmail.com>
2019-11-15 22:56:51 +0200

    Updates

---
+++ precache-manifest.072a4e43fd809aa00dc1ee6afd79f284.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0944502550b59cc464bcd3edd17a73c8",
+    "url": "/react-monsters-app/index.html"
+  },
+  {
+    "revision": "6859e727b2076bcf77b0",
+    "url": "/react-monsters-app/static/css/main.8a2d8944.chunk.css"
+  },
+  {
+    "revision": "66086b389d601cdb11f4",
+    "url": "/react-monsters-app/static/js/2.76508610.chunk.js"
+  },
+  {
+    "revision": "6859e727b2076bcf77b0",
+    "url": "/react-monsters-app/static/js/main.4cbc6197.chunk.js"
+  },
+  {
+    "revision": "275d30955504c1b70739",
+    "url": "/react-monsters-app/static/js/runtime-main.9c410951.js"
+  }
+]);
